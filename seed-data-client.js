d6bfe8432190ba66b6ad5d603e7711eecfa8f3d0
Kelsey Butler <kelseybutler@Kelseys-MacBook-Pro.local>
2018-01-21 19:44:07 -0600

    add client data

---
+++ seed-data-client.js
@@ -0,0 +1,23 @@
+{
+    "first_name": "Kelsey",
+    "last_name": "Butler",
+    "hospital_name": "Matilda",
+    "city": "Columbus",
+    "client_state": "Ohio",
+    "start_date": "2017-06-08T19:30:39+00:00",
+    "end_date": "2017-06-08T19:30:39+00:00",
+    "age": 23,
+    "weight": 130
+}
+
+{
+    "first_name": "Sam",
+    "last_name": "Butler",
+    "hospital_name": "Northwestern",
+    "city": "Chicago",
+    "client_state": "Illinois",
+    "start_date": "2017-06-08T19:30:39+00:00",
+    "end_date": "2017-06-08T19:30:39+00:00",
+    "age": 30,
+    "weight": 135
+}
